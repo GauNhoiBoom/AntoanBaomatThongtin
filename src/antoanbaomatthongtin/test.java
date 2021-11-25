ff573e81deca46c94a2e5ec1c8dd217d922712be
unknown <gaunhoibom1611@gmail.com>
2021-11-25 22:29:29 +0700

    first commit

---
+++ src/antoanbaomatthongtin/test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antoanbaomatthongtin;
+
+/**
+ *
+ * @author nam
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Họ Tên : Đặng Minh Nam");
+        System.out.println("Mã Sinh Viên: 2018603718");
+    }
+}
